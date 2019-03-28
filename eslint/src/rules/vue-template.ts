50540f6fa93971d3bace8e229f43d62d6b1463f1
HaoDaWang <1306036576@qq.com>
2019-03-28 22:36:30 +0800

    feat: 添加 vue-template rule

--- eslint/src/rules/vue-template.ts
+++ eslint/src/rules/vue-template.ts
@@ -2,27 +2,51 @@ import {Rule} from 'eslint'
 import {AST} from 'vue-eslint-parser'
 import {isVElement} from '../utils'
 
+/** default 4 space use tab*/
+const INDENTATION_ERROR_MESSAGE = 'Indentation that is 4 space'
+
+const ROOT_TAG_NAME = 'template'
+
 class Foo implements Rule.RuleModule {
     meta: Rule.RuleMetaData = {
-        messages: {},
+        messages: {
+            indentation: INDENTATION_ERROR_MESSAGE,
+        },
     }
 
-    create(context: Rule.RuleContext): Rule.RuleListener {
-        function VElement(node: AST.VElement) {
-            const {children} = node
+    walker(node: AST.VElement): boolean {
+        if (isVElement(node)) {
+            const col = node.loc.start.column
+            const parentCol = node.parent.loc.start.column
 
-            for (const child of children) {
-                if (isVElement(child)) {
-                    console.info(child.loc.start)
+            if (node.name !== ROOT_TAG_NAME) {
+                // 元素顶格
+                if (col === 0) {
+                    return true
+                }
+                // 空格缩进
+                if (col - parentCol !== 4) {
+                    return true
                 }
             }
         }
 
-        function Program(node: AST.ESLintProgram) {}
+        return false
+    }
+
+    create(context: Rule.RuleContext): Rule.RuleListener {
+        const VElement = (node: AST.VElement) => {
+            if (this.walker(node)) {
+                context.report({
+                    node: node,
+                    messageId: 'indentation',
+                } as any)
+                // vue-eslint-parser cannot provide this type
+            }
+        }
 
         const visitor = {
             VElement,
-            Program,
         }
 
         return context.parserServices.defineTemplateBodyVisitor(visitor)
