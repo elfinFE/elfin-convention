14a912640b46e4bdee6581e4de6730900f8883d5
HaoDaWang <1306036576@qq.com>
2019-04-03 16:37:37 +0800

    test: just demo

--- eslint/src/rules/a.vue
+++ eslint/src/rules/a.vue
@@ -1,10 +1,20 @@
 <template>
-  <div name="foo"></div>
+    <div name="foo"></div>
 </template>
 
 <script>
+import {mapActions} from 'vuex'
+
 export default {
     name: 'bar',
+    methods: {
+        ...mapActions({
+            aaa: 'bbb/sss',
+            vxbbb: 'bbb/sss',
+            ccc: 'bbb/sss',
+        }),
+    },
+    computed: {},
 }
 </script>
 
