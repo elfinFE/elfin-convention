c1db1d76b4bb8cbbb8a48b184d0a7d202ad69ff5
HaoDaWang <1306036576@qq.com>
2019-04-03 14:58:58 +0800

    feat: update type declaration

--- eslint/src/utils/asserts.ts
+++ eslint/src/utils/asserts.ts
@@ -1,7 +1,26 @@
-import {AST} from 'vue-eslint-parser'
+import {NODE_TYPE} from '../types'
+import {
+    ESLintStatement,
+    ESLintModuleDeclaration,
+    ESLintExportDefaultDeclaration,
+    ESLintNode,
+    VElement,
+    VText,
+    VExpressionContainer,
+} from 'vue-eslint-parser/ast'
+
+function compareNodeType(node: ESLintNode, typeName: string): boolean {
+    return node.type === typeName
+}
 
 export function isVElement(
-    target: AST.VElement | AST.VText | AST.VExpressionContainer,
-): target is AST.VElement {
-    return !!(target as AST.VElement).name
+    target: VElement | VText | VExpressionContainer,
+): target is VElement {
+    return !!(target as VElement).name
+}
+
+export function isExportDefaultDeclaration(
+    node: ESLintStatement | ESLintModuleDeclaration,
+): node is ESLintExportDefaultDeclaration {
+    return compareNodeType(node, NODE_TYPE.EXPORT_DEFAULT_DECLARATION)
 }
