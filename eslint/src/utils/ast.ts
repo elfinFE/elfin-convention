cc31fa27f8e65118aee346f8180bbad0c64a662a
HaoDaWang <1306036576@qq.com>
2019-04-03 15:06:44 +0800

    feat: update types

--- eslint/src/utils/ast.ts
+++ eslint/src/utils/ast.ts
@@ -1,8 +1,8 @@
-import {Property, Identifier} from 'estree'
+import {ESLintProperty, ESLintIdentifier} from 'vue-eslint-parser/ast'
 
-export function getNodeName(node: Property): string | undefined {
+export function getNodeName(node: ESLintProperty): string | undefined {
     const {key} = node
-    const {name} = key as Identifier
+    const {name} = key as ESLintIdentifier
 
     return name
 }
