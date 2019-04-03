bbdfb186b7d033ca46326699d0abfba3fbac4eaa
HaoDaWang <1306036576@qq.com>
2019-04-03 20:13:07 +0800

    feat: 添加 elfin/vue-template

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
     extends: ['plugin:vue/essential'],
     rules: {
-        'vue-template': 1,
         'elfin/vue-template':2
     },
-    parser: 'vue-eslint-parser',
+    plugins:[
+        'elfin'
+    ],
 }
