52c1efe0dd57c7370c3f9b3e727bd18f9fd72b51
HaoDaWang <1306036576@qq.com>
2019-04-15 00:25:21 +0800

    feat: 添加 Error Message

--- eslint/src/utils/failure.ts
+++ eslint/src/utils/failure.ts
@@ -1,10 +1,13 @@
-import {Rule} from 'eslint';
+import {Rule} from 'eslint'
+
+export const SYSTEM_COMMENT_POSITION_ERROR_MESSAGE =
+    'System comment must be on the first line of editor'
 
 export function attachFailures(
-    context:Rule.RuleContext,
-    descriptors: Rule.ReportDescriptor[]
-){
-    for(const descriptor of descriptors){
+    context: Rule.RuleContext,
+    descriptors: Rule.ReportDescriptor[],
+) {
+    for (const descriptor of descriptors) {
         context.report(descriptor)
     }
 }
