d52b42d92bf17d55ac6695e251eb0299f4980f73
HaoDaWang <1306036576@qq.com>
2019-04-15 10:48:48 +0800

    feat: 更改 regex 匹配

---
+++ eslint/src/rules/chain-type.ts
@@ -0,0 +1,44 @@
+import {Rule} from 'eslint'
+import {Node} from 'estree'
+import {isSystemComment, SYSTEM_COMMENT_POSITION_ERROR_MESSAGE} from '../utils'
+
+const ROUTER_FILE_NAME = 'router-file'
+
+function isSystemRouterComment(comment: string): boolean {
+    return new RegExp(`^\\$${ROUTER_FILE_NAME}`).test(comment)
+}
+
+/** For webstorm code hint */
+class Chain implements Rule.RuleModule {
+    meta?: Rule.RuleMetaData = {
+        fixable: 'code',
+        messages: {},
+        schema: [{type: 'object', required: true}],
+    }
+
+    // bind this
+    private walkProgram = (node: Node) => {}
+
+    create(context: Rule.RuleContext): Rule.RuleListener {
+        const comments = context.getSourceCode().getAllComments()
+        const firstComment = comments[0]
+        const {value} = firstComment
+        const {
+            start: {line},
+        } = firstComment.loc!
+
+        // system comment must be on the first line of editor
+        if (line !== 1 && isSystemComment(value)) {
+            throw new Error(SYSTEM_COMMENT_POSITION_ERROR_MESSAGE)
+        }
+
+        // collect routes info
+        if (isSystemRouterComment(value)) {
+            console.info('in')
+        }
+
+        return {Program: this.walkProgram}
+    }
+}
+
+module.exports = new Chain()
