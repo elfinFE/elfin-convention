8a3e69e93982f41662068d126a3fdc2bcbf761c4
xiaoyufei <915724526@qq.com>
2019-10-15 17:38:39 +0800

    feat: eslint plugin comments

---
+++ eslint/src/rules/comments.js
@@ -0,0 +1,98 @@
+module.exports = {
+    meta: {
+        type: 'suggestion',
+
+        docs: {
+            description: 'require comments',
+            category: 'Stylistic Issues',
+            recommended: false,
+            url: 'https://github.com/elfinFE/elfin-convention.git'
+        },
+
+        schema: [
+            {
+                type: 'object',
+                properties: {
+                    require: {
+                        type: 'object',
+                        properties: {
+                            FunctionDeclaration: {
+                                type: 'boolean',
+                                default: true
+                            },
+                            FunctionExpression: {
+                                type: 'boolean',
+                                default: true
+                            }
+                        },
+                        additionalProperties: false,
+                        default: {}
+                    }
+                },
+                additionalProperties: false
+            }
+        ],
+        deprecated: true,
+        replacedBy: []
+    },
+
+    create(context) {
+        const source = context.getSourceCode();
+        const DEFAULT_OPTIONS = {
+            FunctionDeclaration: true,
+            FunctionExpression: true
+        };
+        const options = Object.assign(DEFAULT_OPTIONS, context.options[0] && context.options[0].require);
+        const fnDeclaration = ['Program', 'FunctionDeclaration', 'FunctionExpression', 'ArrowFunctionExpression']
+
+        /**
+         * report错误信息
+         * @param {Node} node
+         */
+        function report(node) {
+            context.report({node, message: 'Missing function comment.'});
+        }
+
+        /**
+         * 校验是否有注释
+         * @param {Node} node
+         */
+        function checkComments(node) {
+            const jsComment = source.getComments(node);
+            const jsDocComment = source.getJSDocComment(node);
+
+            if ((!jsComment || jsComment.leading.length === 0) && !jsDocComment) {
+                report(node);
+            }
+        }
+
+        /**
+         * 是否是在外层定义 (不是嵌套定义)
+         * @param {Object} node
+         */
+        function isOuterDeclaration(node) {
+            let ancestor = node.parent
+            while (ancestor && fnDeclaration.indexOf(ancestor.type) < 0) {
+                ancestor = ancestor.parent;
+            }
+
+            return !ancestor || ancestor.type === 'Program';
+        }
+
+        return {
+            // 函数定义 function bar() {}
+            FunctionDeclaration(node) {
+                if (options.FunctionDeclaration && isOuterDeclaration(node)) {
+                    checkComments(node);
+                }
+            },
+            // 函数表达式 var bar = function() {}
+            FunctionExpression(node) {
+                // 父级是 变量定义的形式
+                if (options.FunctionExpression && (node.parent.type === 'VariableDeclarator') && isOuterDeclaration(node)) {
+                    checkComments(node);
+                }
+            },
+        };
+    }
+};
---
+++ eslint/src/tests/rules/comments.ts
@@ -0,0 +1,59 @@
+import {EslintTest, MetaData} from '../../utils'
+import comments from '../../rules/comments'
+import {RuleTester} from 'eslint'
+
+class FunctionNoteTest extends EslintTest {
+    metaData: MetaData = {
+        rule: comments,
+        testName: 'comments',
+    }
+
+    valid(): string | (string | RuleTester.ValidTestCase)[] {
+        return [
+            "// Description\nfunction bar() {}",
+            "/**\n* Description\n */\nfunction bar() {}",
+            "/**\nDescription\n*/\nfunction bar() {}",
+            "/**\nDescription\n*/\nvar bar = function() {}",
+            "var bar = {bar1: function() {}}",
+            "// Description\nfunction bar() {function bar1() {}}",
+            "var bar = {bar1: function() {}}",
+            `export default {
+                data() {
+                    return {}
+                },
+                computed: {
+                    bar() {},
+                },
+                methods: {
+                    bar() {},
+                },
+                mounted() {},
+            }`,
+        ]
+    }
+
+    invalid(): RuleTester.InvalidTestCase[] {
+        return [
+            {
+                code: "function bar() {}",
+                errors: [{message: "Missing function comment."}],
+            },
+            {
+                code: "var bar = function() {}",
+                errors: [{message: "Missing function comment."}],
+            },
+            {
+                code: "function bar() {function bar1() {}}",
+                errors: [{message: "Missing function comment."}],
+            },
+        ]
+    }
+}
+
+new FunctionNoteTest({
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+}).runTest()
