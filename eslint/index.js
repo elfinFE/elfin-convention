8634c87e053594a817a920e82d0fc37083a7389a
xiaoyufei <915724526@qq.com>
2019-10-16 11:28:22 +0800

    feat: .npmignore

--- eslint/index.js
+++ eslint/index.js
@@ -1,7 +1,11 @@
 const VxPrefix = require('./dist/rules/vx-prefix')
 const RefPrefix = require('./dist/rules/ref-prefix')
+const Comments = require('./dist/rules/comments')
+const InvalidConsole = require('./dist/rules/invalid-console')
 
 module.exports = {
     VxPrefix,
     RefPrefix,
+    Comments,
+    InvalidConsole,
 }
